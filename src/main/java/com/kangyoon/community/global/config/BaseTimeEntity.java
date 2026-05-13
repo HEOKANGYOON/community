3db203a4eb68cf03dab9d097f28c20ee233a3642
kangyoon <kangyoob@local>
2026-05-13 20:26:26 +0900

    chore: add BaseEntity and JPA Auditing config

---
+++ src/main/java/com/kangyoon/community/global/config/BaseTimeEntity.java
@@ -0,0 +1,18 @@
+package com.kangyoon.community.global.config;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/com/kangyoon/community/global/config/JpaAuditingConfig.java
@@ -0,0 +1,20 @@
+package com.kangyoon.community.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+
+    @Bean
+    public DateTimeProvider auditingDateTimeProvider() {
+        return () -> Optional.of(LocalDateTime.now(ZoneId.of("Aisa/Seoul")));
+    }
+}
