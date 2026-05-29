488ac13e4bef1db83132f23e36fa460e39ed47a2
kangyoon <kangyoob@local>
2026-05-29 20:16:08 +0900

    feat: add comment related error code

--- src/main/java/com/kangyoon/community/global/exception/ErrorCode.java
+++ src/main/java/com/kangyoon/community/global/exception/ErrorCode.java
@@ -44,7 +44,9 @@ public enum ErrorCode {
 
     /*Comment*/
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "CM001", "존재하지 않는 댓글입니다."),
-    COMMENT_AUTHOR_MISMATCH(HttpStatus.FORBIDDEN, "CM002", "댓글 작성자가 아닙니다.");
+    COMMENT_AUTHOR_MISMATCH(HttpStatus.FORBIDDEN, "CM002", "댓글 작성자가 아닙니다."),
+    REPLY_DEPTH_EXCEEDED(HttpStatus.FORBIDDEN, "CM003", "원댓글에만 대댓글을 허용합니다.");
+
 
 
     private final HttpStatus status;
