7f66ae783e4ac228c7bbefb11f0a313e183f8e02
kangyoon <kangyoob@local>
2026-07-07 18:59:35 +0900

    fix: add FULLTEXT INDEX (title), (title, content) with ngram parser to schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -137,6 +137,6 @@ ALTER TABLE comment_like ADD FOREIGN KEY (comment_id) REFERENCES comment(id);
 
 ALTER TABLE notification ADD FOREIGN KEY (member_id) REFERENCES member(id);
 
-ALTER TABLE post ADD FULLTEXT INDEX idx_ft_title (title);
+ALTER TABLE post ADD FULLTEXT INDEX idx_title (title) WITH PARSER ngram;
 
-ALTER TABLE post ADD FULLTEXT INDEX idx_ft_title_content (title, content);
+ALTER TABLE post ADD FULLTEXT INDEX idx_title_content (title, content) WITH PARSER ngram;
