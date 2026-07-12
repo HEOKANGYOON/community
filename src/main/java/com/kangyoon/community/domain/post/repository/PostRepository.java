123800f9927c7d70bb5b2c4bc201dae9eac6a1b4
kangyoon <kangyoob@local>
2026-07-12 18:10:59 +0900

    refactor: Change paging repository parameter type from PageRequest to Pageable

--- src/main/java/com/kangyoon/community/domain/post/repository/PostRepository.java
+++ src/main/java/com/kangyoon/community/domain/post/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.kangyoon.community.domain.post.repository;
 import com.kangyoon.community.domain.post.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
@@ -10,5 +11,5 @@ import java.util.Optional;
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
     @EntityGraph(attributePaths = {"member"})
     Optional<Post> findByIdAndDeletedAtIsNull(Long id);
-    Page<Post> findAllByDeletedAtIsNull(PageRequest pageable);
+    Page<Post> findAllByDeletedAtIsNull(Pageable pageable);
 }
